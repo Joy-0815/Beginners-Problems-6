ae4926749b72ff662b9dd8eadd0281d41c1576ac
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-21 18:31:37 +1200

    Create tri.py

---
+++ tri.py
@@ -0,0 +1,8 @@
+def triangleTest(side1, side2, side3):
+    to = side1 + side2
+    if to > side3 :
+        print("It's a triangle.")
+    else:
+        print("It's not a triangle.")
+
+triangleTest("3","5","4")
